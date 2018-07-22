92d84c18d2c4960373a5ff8189a76e09204aef8f
cchira <cristichira1@gmail.com>
2018-07-22 15:49:49 +0300

    implemented spring security

--- src/main/java/com/app/office/user/impl/service/UserMapper.java
+++ src/main/java/com/app/office/user/impl/service/UserMapper.java
@@ -4,12 +4,18 @@ import com.app.office.shared.api.dto.NameIdDTO;
 import com.app.office.shared.impl.NameIdMapper;
 import com.app.office.user.api.dto.UserDTO;
 import com.app.office.user.domain.User;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Mapper(uses = {NameIdMapper.class})
 public abstract class UserMapper {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Mapping(target = "name", source = "email")
     abstract NameIdDTO toNameIdDTO(User user);
@@ -17,5 +23,13 @@ public abstract class UserMapper {
     abstract UserDTO toDTO(User user);
 
     @Mapping(target = "managerId", ignore = true)
+    @Mapping(target = "password", ignore = true)
     abstract User toEntity(UserDTO userDTO, @MappingTarget User user);
+
+    @AfterMapping
+    void afterMapToEntity(UserDTO userDTO, @MappingTarget User user) {
+        if (userDTO.getPassword() != null) {
+            user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        }
+    }
 }
